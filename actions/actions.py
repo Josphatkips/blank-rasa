56e4b51a52832fe2f143bc99547ef9dd23a49bbc
Josphat Kipyegon <josphatkips@gmail.com>
2022-05-01 21:32:08 +0300

    updates

--- actions/actions.py
+++ actions/actions.py
@@ -12,6 +12,7 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import AllSlotsReset,FollowupAction
 import requests
 authurl = 'https://shop.roycetechnologies.co.ke/wp-json/jwt-auth/v1/token'
 # authurl = 'https://shop.roycetechnologies.co.ke/wp-json/cocart/v2/login'
@@ -36,25 +37,27 @@ class ActionHelloWorld(Action):
 
        
         
-        # response = requests.post(authurl, json=data)
+        response = requests.post(authurl, json=data)
        
-        # json_response=response.json()
+        json_response=response.json()
        
 
-        # print (json_response['success'])
+        print (json_response['success'])
         
-        # if json_response['success']==False :
-        if 2==3 :
-            print('yes')
+        if json_response['success']==False :
+
+        
             # wrong password
-            # dispatcher.utter_message(text=json_response['message'])
+            dispatcher.utter_message(text=json_response['message'])
+            
+            return [AllSlotsReset(),FollowupAction(name = "name_form")]
         else:
             # right password query cart
             # 
-            # token= json_response['data']['token']
-            token= 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvc2hvcC5yb3ljZXRlY2hub2xvZ2llcy5jby5rZSIsImlhdCI6MTY1MTQyNDEwNywibmJmIjoxNjUxNDI0MTA3LCJleHAiOjE2NTIwMjg5MDcsImRhdGEiOnsidXNlciI6eyJpZCI6MiwiZGV2aWNlIjoiIiwicGFzcyI6ImU5NjRlODY3ZTlkMjBiOWQwNjQxOGZkMDJhNzgyNTk3In19fQ.nus-YXBKxuNYJKSRmHoX8bq9gMYd7WN0Ec13lzbPZyM'
-            # first_name= json_response['data']['firstName']
-            first_name= 'Josphat'
+            token= json_response['data']['token']
+            # token= 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvc2hvcC5yb3ljZXRlY2hub2xvZ2llcy5jby5rZSIsImlhdCI6MTY1MTQyNDEwNywibmJmIjoxNjUxNDI0MTA3LCJleHAiOjE2NTIwMjg5MDcsImRhdGEiOnsidXNlciI6eyJpZCI6MiwiZGV2aWNlIjoiIiwicGFzcyI6ImU5NjRlODY3ZTlkMjBiOWQwNjQxOGZkMDJhNzgyNTk3In19fQ.nus-YXBKxuNYJKSRmHoX8bq9gMYd7WN0Ec13lzbPZyM'
+            first_name= json_response['data']['firstName']
+            # first_name= 'Josphat'
 
             dispatcher.utter_message(text="Hello "+first_name+" wait a moment as I process your request.")
 
